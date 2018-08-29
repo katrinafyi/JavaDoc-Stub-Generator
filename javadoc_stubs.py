06b2be01cf3e738b56b39959632b263483ae367e
Kenton Lam <kenton_lam@outlook.com>
2018-08-29 16:48:49 +1000

    Implement command line arguments.

--- javadoc_stubs.py
+++ javadoc_stubs.py
@@ -28,18 +28,18 @@ class JavaClass:
     def add_method(self, method):
         self._methods.append(method)
 
-    def format(self):
+    def format_as_lines(self, indent=4):
         is_interface = 'interface ' in self._definition
         ret = [javadoc_comment(self._desc), self._definition + ' {']
         ret.append('')
         for c in self._constructors + self._methods:
-            ret.extend('    '+x for x in c.format(is_interface))
+            ret.extend(indent*' '+x for x in c.format_as_lines(is_interface))
             ret.append('')
         ret.append('}')
         return ret
 
-    def str_format(self):
-        return '\n'.join(self.format())
+    def format(self, indent=4):
+        return '\n'.join(self.format_as_lines(indent=indent))
 
     __repr__ = __str__ = _lazy_str
 
@@ -60,7 +60,7 @@ class JavaMethod:
     def set_description(self, description):
         self._description = description
 
-    def format(self, interface=False):
+    def format_as_lines(self, interface=False):
         return (
             javadoc_comment(self._description, self._at_tags) + '\n' +
             (self._definition + (';' if interface else ' {}'))
@@ -150,8 +150,30 @@ class JavaDocParser:
 
 
 if __name__ == '__main__':
-    out = JavaDocParser().parse_folder(os.path.dirname(__file__) + '/../Csse2002_Ass1/javadoc2')
+    import argparse
+    import errno
+
+    arg_parser = argparse.ArgumentParser(
+        description='Generates Java method stubs from a JavaDoc folder.')
+    arg_parser.add_argument('INPUT', nargs='+', 
+        help='input JavaDoc HTML files or directory')
+    arg_parser.add_argument('OUTPUT', nargs='?', default='./stub', 
+        help='output directory (default: ./stub/)')
+    arg_parser.add_argument('--indent', '-i', type=int, default=4,
+        help='number of spaces to use for indentation (default: 4)')
+    args = arg_parser.parse_args()
+    
+    javadoc_parser = JavaDocParser()
+    out = []
+
+    for file_or_dir in args.INPUT:
+        try:
+            out.extend(javadoc_parser.parse_folder(file_or_dir))
+        except NotADirectoryError:
+            out.append(javadoc_parser.parse_file(file_or_dir))
+
+    os.makedirs(args.OUTPUT, exist_ok=True)
 
     for c in out:
-        with open(os.path.dirname(__file__) + '/../Csse2002_Ass1/stub/'+c.name+'.java', 'w', encoding='utf-8') as f:
-            f.write(c.str_format().replace('\xa0', ' ').replace(' ', ' '))
+        with open(os.path.join(args.OUTPUT, c.name+'.java'), 'w', encoding='utf-8') as f:
+            f.write(c.format().replace('\xa0', ' ').replace(' ', ' '))
